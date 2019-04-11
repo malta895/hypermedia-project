6838e7a018cf6e3452149e937f164a441215eca2
malta895 <malta95@hotmail.it>
2019-04-11 11:26:46 +0200

    First test of serving static template

---
+++ index.js
@@ -0,0 +1,16 @@
+let path = require('path');
+let http = require('http');
+
+let serveStatic = require('serve-static');
+
+let app = require('connect')();
+
+const serverPort = 8080;
+
+app.use(serveStatic(__dirname + "/public"));
+
+http.createServer(app).listen(serverPort, function() {
+    console.log(`Server listening on ${serverPort}`);
+})
+
+
