85107102f48bbcd5a1b9d054012c7b2eaff6b57d
Luca Maltagliati <malta95@hotmail.it>
2019-06-27 14:21:23 +0200

    Bugfix

---
+++ controllers/Genre.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var utils = require('../utils/writer.js');
+var Genre = require('../service/GenreService');
+
+module.exports.genresGET = function genresGET (req, res, next) {
+    var offset = req.swagger.params['offset'].value;
+    var limit = req.swagger.params['limit'].value;
+    Genre.genresGET(offset,limit)
+        .then(function (response) {
+            utils.writeJson(res, response);
+        })
+        .catch(function (response) {
+            console.error(response);
+            utils.writeJson(res,{message: "Internal Server Error!"} , 500);
+        });
+};
